be7d8d5ee089edcb76c9e54036152d41c4dc1e65
Tomasz Kucinski <Tomasz.Kucinski@brainproducts.com>
2019-12-11 11:29:51 +0100

    BrainVision to BIDS Converter exposed as command line application

---
+++ src/FileFormats/src/FileFormats.BrainVisionToBidsConverterCLI/Internal/CommandLineParser.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using BrainVision.Lab.FileFormats.Properties;
+
+namespace BrainVision.Lab.FileFormats
+{
+    internal class CommandLineParser
+    {
+        // using Apache naming convention for command line arguments. MS dotnet CLI uses this standard as well.
+        // see: https://commons.apache.org/proper/commons-cli/
+
+        private readonly Option[] _parameterlessOptions = new Option[] { Option.Help, Option.Version }; // options that may be parameterless
+
+        private readonly string[] _args;
+        public CommandLineParser(string[] args)
+            => _args = args;
+
+        public bool HelpRequested { get; private set; } = false;
+        public Option? HelpTopic { get; private set; }
+        public bool VersionRequested { get; private set; } = false;
+
+        public string? BrainVisionHeaderFilePath { get; private set; }
+        public string? DestinationFolderPath { get; private set; }
+        public CustomizationInfo CustomizationInfo { get; } = new CustomizationInfo();
+
+        public enum Option
+        {
+            // general purpose options
+            Help,
+            Version,
+            // required options (must be provided in command-line)
+            Src,
+            Dst,
+            // optional options (may be provided in command-line, otherwise default values will be applied)
+            Subject,
+            Session,
+            Task,
+            DatasetName,
+            License,
+            Authors,
+            Manufacturer,
+            PowerLineFrequency,
+            EEGReference,
+        }
+
+        public static string?[] OptionLongNames { get; } = new string?[]
+        {
+            "--help",
+            "--version",
+            "--bv-header-file",
+            "--bids-destination-folder",
+            "--subject",
+            "--session",
+            "--task",
+            "--paradigm",
+            "--license",
+            "--authors",
+            "--manufacturer",
+            "--power-line-frequency",
+            "--EEG-reference"
+        };
+
+        public static string?[] OptionShortNames { get; } = new string?[]
+        {
+            "-h",
+            "-v",
+            "-hdr",
+            "-dst",
+            "-sub",
+            "-ses",
+            "-tsk",
+            "-par",
+            "-lic",
+            "-aut",
+            "-mnf",
+            "-plf",
+            "-ref"
+        };
+
+        public void ParseCommandLine()
+        {
+            Debug.Assert(OptionLongNames.Length == Enum.GetValues(typeof(Option)).Length);
+            Debug.Assert(OptionShortNames.Length == Enum.GetValues(typeof(Option)).Length);
+
+            // checking whether duplicated strings do not occur
+            Debug.Assert(OptionLongNames.Length == OptionLongNames.Distinct().Count(), "Some long options are duplicated");
+            Debug.Assert(OptionShortNames.Length == OptionShortNames.Distinct().Count(), "Some short options are duplicated");
+
+            List<Option> alreadyParsedOptions = new List<Option>();
+
+            for (int i = 0; i < _args.Length; i++)
+            {
+                string arg = _args[i];
+
+                Option option = ParseOptionArg(arg);
+
+                if (alreadyParsedOptions.Contains(option))
+                {
+                    string text = $"{Resources.ExceptionTextParamDuplicated} {arg}";
+                    throw new InvalidOperationException(text);
+                }
+
+                i += ParseOptionValues(i, option);
+
+                alreadyParsedOptions.Add(option);
+            }
+        }
+
+        private static Option ParseOptionArg(string arg)
+        {
+            int optionIndex = Array.FindIndex(OptionShortNames, p => p == arg);
+            if (optionIndex < 0)
+            {
+                optionIndex = Array.FindIndex(OptionLongNames, p => p == arg);
+
+                if (optionIndex < 0)
+                {
+                    string text = $"{Resources.ExceptionTextParamUnrecognized} {arg}.";
+                    throw new InvalidOperationException(text);
+                }
+            }
+
+            return (Option)optionIndex;
+        }
+
+        private static bool IsValidOption(string s)
+            => OptionShortNames.Contains(s) || OptionLongNames.Contains(s);
+
+        private static double ParseArgumentAsDouble(string s, string name)
+        {
+            if (double.TryParse(s, NumberStyles.Float, CultureInfo.InvariantCulture, out double val))
+                return val;
+
+            string text = $"{Resources.ExceptionTextParamValueUnrecognized} {name} {s}. {Resources.ExceptionTextExpectedFloat}.";
+            throw new InvalidOperationException(text);
+        }
+
+        private List<string> ParseArgumentsAsListTillNextValidOptionFound(int indexOfFirstArgumentToParse)
+        {
+            List<string> list = new List<string>();
+
+            for (int a = indexOfFirstArgumentToParse; a < _args.Length; ++a)
+            {
+                string s = _args[a];
+                if (IsValidOption(s))
+                    break;
+
+                list.Add(s.Trim('"'));
+            }
+
+            return list;
+        }
+
+        // I leave this commented methods in case I need them to parse an enum:
+
+        //private static T ParseEnum<T>(string textToParse, string paramName) where T : struct, Enum, IConvertible // Enum is a struct with IConvertible interface
+        //{
+        //    if (Enum.TryParse(textToParse, out T value)) // case sensitive
+        //        return value;
+
+        //    string[] names = Enum.GetNames(typeof(T));
+        //    string commaSeparated = ToCommaSeparatedString(names);
+        //    string text = $"{Resources.ExceptionTextParamValueUnrecognized} {paramName} {textToParse}. {Resources.ExceptionTextExpectedEnum} {commaSeparated}.";
+        //    throw new InvalidOperationException(text);
+        //}
+
+        //private static string ToCommaSeparatedString(string[] names)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+
+        //    foreach (string name in names)
+        //    {
+        //        if (sb.Length != 0)
+        //            sb.Append(", ");
+        //        sb.Append(name);
+        //    }
+
+        //    return sb.ToString();
+        //}
+
+        /// <summary>
+        /// <returns>returns how many option line arguments have been parsed for this option</returns>
+        /// <exception cref="InvalidOperationException">Thrown when requested argument's value has a wrong format.</exception>
+        private int ParseOptionValues(int i, Option option)
+        {
+            if (!_parameterlessOptions.Contains(option))
+            {
+                if (i + 1 >= _args.Length)
+                {
+                    string text = $"{Resources.ExceptionTextParamValueMissing} {_args[i]}.";
+                    throw new InvalidOperationException(text);
+                }
+            }
+
+            switch (option)
+            {
+                case Option.Help:
+                    {
+                        HelpRequested = true;
+
+                        if (i + 1 < _args.Length)
+                        {
+                            string nextItem = _args[i + 1];
+                            if (IsValidOption(nextItem))
+                            {
+                                HelpTopic = ParseOptionArg(nextItem);
+                                return 1;
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException($"{Resources.ExceptionTextParamValueUnrecognized} {_args[i]} {_args[i + 1]}. {Resources.ExceptionTextExpectedValidOptionOrEmpty}.");
+                            }
+                        }
+
+                        return 0;
+                    }
+                case Option.Version:
+                    {
+                        VersionRequested = true;
+                        return 0;
+                    }
+                case Option.Src:
+                    {
+                        BrainVisionHeaderFilePath = _args[i + 1];
+
+                        if (string.IsNullOrWhiteSpace(BrainVisionHeaderFilePath))
+                            throw new InvalidOperationException($"{Resources.ExceptionTextParamEmpty} {_args[i]}");
+
+                        return 1;
+                    }
+                case Option.Dst:
+                    {
+                        DestinationFolderPath = _args[i + 1];
+
+                        if (string.IsNullOrWhiteSpace(DestinationFolderPath))
+                            throw new InvalidOperationException($"{Resources.ExceptionTextParamEmpty} {_args[i]}");
+
+                        return 1;
+                    }
+                case Option.DatasetName:
+                    {
+                        CustomizationInfo.DatasetName = _args[i + 1];
+                        // throws no exception. Empty value is accepted
+                        return 1;
+                    }
+                case Option.Subject:
+                    {
+                        CustomizationInfo.SubjectName = _args[i + 1];
+
+                        if (string.IsNullOrWhiteSpace(CustomizationInfo.SubjectName))
+                            throw new InvalidOperationException($"{Resources.ExceptionTextParamEmpty} {_args[i]}");
+
+                        return 1;
+                    }
+                case Option.Session:
+                    {
+                        CustomizationInfo.SessionName = _args[i + 1];
+
+                        if (string.IsNullOrWhiteSpace(CustomizationInfo.SessionName))
+                            throw new InvalidOperationException($"{Resources.ExceptionTextParamEmpty} {_args[i]}");
+
+                        return 1;
+                    }
+                case Option.Task:
+                    {
+                        CustomizationInfo.TaskName = _args[i + 1];
+
+                        if (string.IsNullOrWhiteSpace(CustomizationInfo.TaskName))
+                            throw new InvalidOperationException($"{Resources.ExceptionTextParamEmpty} {_args[i]}");
+
+                        return 1;
+                    }
+                case Option.License:
+                    {
+                        // throws no exception. Empty value is accepted
+                        CustomizationInfo.License = _args[i + 1];
+                        return 1;
+                    }
+                case Option.Authors: // space separated items
+                    {
+                        // throws no exception. Empty value is accepted
+                        CustomizationInfo.Authors = ParseArgumentsAsListTillNextValidOptionFound(i + 1);
+                        return CustomizationInfo.Authors.Count;
+                    }
+                case Option.Manufacturer:
+                    {
+                        // throws no exception. Empty value is accepted
+                        CustomizationInfo.Manufacturer = _args[i + 1];
+                        return 1;
+                    }
+                case Option.EEGReference:
+                    {
+                        CustomizationInfo.EEGReference = _args[i + 1];
+
+                        if (string.IsNullOrWhiteSpace(CustomizationInfo.EEGReference))
+                            throw new InvalidOperationException($"{Resources.ExceptionTextParamEmpty} {_args[i]}");
+
+                        return 1;
+                    }
+                case Option.PowerLineFrequency:
+                    {
+                        CustomizationInfo.PowerLineFrequency = ParseArgumentAsDouble(_args[i + 1], _args[i]);
+
+                        if (CustomizationInfo.PowerLineFrequency <= 0.0)
+                            throw new InvalidOperationException($"{Resources.ExceptionTextParamZeroOrNegative} {_args[i]}");
+
+                        return 1;
+                    }
+                default:
+                    throw new NotImplementedException(); // should never happen
+            }
+        }
+
+        public void ThrowExceptionIfRequiredParameterMissing()
+        {
+            if (BrainVisionHeaderFilePath == null)
+                throw new InvalidOperationException($"{Resources.ExceptionTextRequiredParamMissing} {CombinedOptionSyntax(Option.Src)}");
+
+            if (DestinationFolderPath == null)
+                throw new InvalidOperationException($"{Resources.ExceptionTextRequiredParamMissing} {CombinedOptionSyntax(Option.Dst)}");
+        }
+
+        private static string CombinedOptionSyntax(Option option)
+            => $"{OptionShortNames[(int)option]}|{OptionLongNames[(int)option]}";
+    }
+}
---
+++ src/FileFormats/src/FileFormats.BrainVisionToBidsConverterCLI/Program.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using BrainVision.Lab.FileFormats.Properties;
+
+namespace BrainVision.Lab.FileFormats
+{
+    public static class Program
+    {
+        const int ReturnValueOnSuccess = 0;
+        const int ReturnValueOnFailure = -1;
+
+        public static int Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (TryParseCommandLine(args, out string? brainVisionHeaderFilePath, out string? destinationFolderPath, out CustomizationInfo? info, out bool helpRequested, out CommandLineParser.Option? helpTopic, out bool versionRequested))
+                {
+                    if (helpRequested)
+                    {
+                        if (helpTopic.HasValue)
+                        {
+                            WriteHelpToConsole(helpTopic.Value);
+                        }
+                        else
+                        {
+                            WriteHelpToConsole();
+                        }
+
+                        return ReturnValueOnSuccess;
+                    }
+                    else if (versionRequested)
+                    {
+                        WriteProductVersionToConsole();
+                        return ReturnValueOnSuccess;
+                    }
+                    else if (Convert(brainVisionHeaderFilePath, destinationFolderPath, info))
+                    {
+                        return ReturnValueOnSuccess;
+                    }
+                    else
+                    {
+                        return ReturnValueOnFailure;
+                    }
+                }
+            }
+            else
+            {
+                WriteHelpToConsole();
+            }
+
+            return ReturnValueOnFailure;
+        }
+
+        private static bool TryParseCommandLine(string[] args, [NotNullWhen(true)] out string? brainVisionHeaderFilePath, [NotNullWhen(true)] out string? destinationFolderPath, [NotNullWhen(true)] out CustomizationInfo? info, out bool helpRequested, [NotNullWhen(true)] out CommandLineParser.Option? helpTopic, out bool versionRequested)
+        {
+            CommandLineParser commandLineParser = new CommandLineParser(args);
+            try
+            {
+                commandLineParser.ParseCommandLine();
+
+                helpRequested = commandLineParser.HelpRequested;
+                helpTopic = commandLineParser.HelpTopic;
+                versionRequested = commandLineParser.VersionRequested;
+
+                if (helpRequested || versionRequested)
+                {
+                    // when help or version is requested, the command line is ignored, even if it is correct
+                    brainVisionHeaderFilePath = null;
+                    destinationFolderPath = null;
+                    info = null;
+
+                    return true;
+                }
+                else
+                {
+                    commandLineParser.ThrowExceptionIfRequiredParameterMissing();
+
+                    brainVisionHeaderFilePath = commandLineParser.BrainVisionHeaderFilePath;
+                    destinationFolderPath = commandLineParser.DestinationFolderPath;
+                    info = commandLineParser.CustomizationInfo;
+
+                    return true;
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine($"{Resources.ConsoleOutputInvalidSyntax} {e.Message}");
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception e) // all possible exceptions must be caught, program may not crash
+            {
+                Console.WriteLine($"{Resources.ConsoleOutputUnexpectedError} {e.Message}");
+            }
+#pragma warning restore CA1031 // Do not catch general exception types
+
+            helpRequested = false;
+            helpTopic = null;
+            versionRequested = false;
+
+            brainVisionHeaderFilePath = null;
+            destinationFolderPath = null;
+            info = null;
+
+            return false;
+        }
+
+        private static bool Convert(string brainVisionHeaderFilePath, string destinationFolderPath, CustomizationInfo info)
+        {
+            try
+            {
+                BrainVisionToBidsConverter converter = new BrainVisionToBidsConverter(destinationFolderPath);
+                converter.Convert(brainVisionHeaderFilePath, info);
+                return true;
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception e) // all possible exceptions must be caught, program may not crash
+            {
+                Console.WriteLine($"{Resources.ConsoleOutputConversionFailed} {e.Message}");
+                return false;
+            }
+#pragma warning restore CA1031 // Do not catch general exception types
+        }
+
+        private static void WriteHelpToConsole(CommandLineParser.Option option)
+        {
+            string optionStr = BuildHelpOptionNameColumn(option);
+            string helpText = GetHelpOptionText(option).Replace("\\n", Environment.NewLine, StringComparison.Ordinal);
+            string optionStrAndHelpText = $"{optionStr}{Environment.NewLine}{helpText}";
+
+            Console.Write(optionStrAndHelpText);
+        }
+
+        private static void WriteHelpToConsole()
+        {
+            BuildOptionInfo(out List<string> requiredOptions, out List<string> optionalOptions, out List<string> generalPurposeOptions);
+
+            string fullPath = Assembly.GetExecutingAssembly().Location;
+            string appName = Path.GetFileNameWithoutExtension(fullPath);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Resources.HelpMessageUsage);
+            sb.AppendLine($" {appName} {Resources.HelpMessageRequiredParamsInCommandline} [{Resources.HelpMessageOptionalParamsInCommandline}]");
+            sb.AppendLine($" {appName} -h");
+            sb.AppendLine($" {appName} -h -hdr");
+            sb.AppendLine($" {appName} --version");
+
+            sb.AppendLine();
+            sb.AppendLine(Resources.HelpMessageGeneralPurposeParams);
+
+            foreach (string generalPurposeOption in generalPurposeOptions)
+                sb.AppendLine(generalPurposeOption);
+
+            sb.AppendLine();
+            sb.AppendLine(Resources.HelpMessageRequiredParams);
+
+            foreach (string requiredOption in requiredOptions)
+                sb.AppendLine(requiredOption);
+
+            sb.AppendLine();
+            sb.AppendLine(Resources.HelpMessageOptionalParams);
+
+            foreach (string optionalOption in optionalOptions)
+                sb.AppendLine(optionalOption);
+
+            Console.Write(sb);
+        }
+
+        private static void BuildOptionInfo(out List<string> requiredOptions, out List<string> optionalOptions, out List<string> generalPurposeOptions)
+        {
+            generalPurposeOptions = new List<string>();
+            requiredOptions = new List<string>();
+            optionalOptions = new List<string>();
+
+            CommandLineParser.Option[] options = (CommandLineParser.Option[])Enum.GetValues(typeof(CommandLineParser.Option));
+            for (int i = 0; i < options.Length; ++i)
+            {
+                CommandLineParser.Option option = (CommandLineParser.Option)i;
+
+                string optionStr = BuildHelpOptionNameColumn(option);
+
+                string helpText = GetHelpOptionText(option);
+                int newLineIndex = helpText.IndexOf("\\n", StringComparison.Ordinal);
+                string firstLineOfHelpText = newLineIndex < 0 ? helpText : helpText[0..newLineIndex];
+
+                string optionStrAndHelpText = $" {optionStr,-32}{firstLineOfHelpText}";
+
+                bool isGeneralPurpose = i < 2;
+                
+                if (isGeneralPurpose)
+                {
+                    generalPurposeOptions.Add(optionStrAndHelpText);
+                }
+                else
+                {
+                    bool isRequired = i < 4;
+
+                    if (isRequired)
+                        requiredOptions.Add(optionStrAndHelpText);
+                    else
+                        optionalOptions.Add(optionStrAndHelpText);
+                }
+            }
+        }
+
+        private static string BuildHelpOptionNameColumn(CommandLineParser.Option option)
+        {
+            string? optionShort = CommandLineParser.OptionShortNames[(int)option];
+            string? optionLong = CommandLineParser.OptionLongNames[(int)option];
+            bool bothPresent = (optionShort != null) && (optionLong != null);
+
+            string? separator = bothPresent ? "|" : null;
+
+            string optionStr = $"{optionShort}{separator}{optionLong}";
+            return optionStr;
+        }
+
+        private static string GetHelpOptionText(CommandLineParser.Option option)
+        {
+            return option switch
+            {
+                CommandLineParser.Option.Help => Resources.HelpMessageHelp,
+                CommandLineParser.Option.Version => Resources.HelpMessageVersion,
+                CommandLineParser.Option.Src => Resources.HelpMessageSrc,
+                CommandLineParser.Option.Dst => Resources.HelpMessageDst,
+                CommandLineParser.Option.Subject => Resources.HelpMessageSubject,
+                CommandLineParser.Option.Session => Resources.HelpMessageSession,
+                CommandLineParser.Option.Task => Resources.HelpMessageTask,
+                CommandLineParser.Option.DatasetName => Resources.HelpMessageDataset,
+                CommandLineParser.Option.License => Resources.HelpMessageLicense,
+                CommandLineParser.Option.Authors => Resources.HelpMessageAuthors,
+                CommandLineParser.Option.Manufacturer => Resources.HelpMessageManufacturer,
+                CommandLineParser.Option.PowerLineFrequency => Resources.HelpMessagePowerLineFreq,
+                CommandLineParser.Option.EEGReference => Resources.HelpMessageEegReference,
+                _ => throw new NotImplementedException() //should never happen
+            };
+        }
+
+        private static void WriteProductVersionToConsole()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string? informationalVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            Console.WriteLine(informationalVersion);
+        }
+    }
+}
